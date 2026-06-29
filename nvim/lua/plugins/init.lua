41e83da0d5a86e84a43c04c3ff7d14c5698dad12
Erik Canton <erikecanton@gmail.com>
2026-06-29 14:23:18 -0500

    errors

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -8,6 +8,7 @@ return {
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
+    opts = { diagnostics = { virtual_text = false } },
     config = function()
       require "configs.lspconfig"
     end,
@@ -16,48 +17,67 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "clangd"
-      }
-    }
+        "clangd",
+      },
+    },
   },
   {
     "mfussenegger/nvim-dap",
     dependencies = {
       "rcarriga/nvim-dap-ui", -- Optional, for a better UI
-      "nvim-neotest/nvim-nio"
+      "nvim-neotest/nvim-nio",
     },
     config = function()
       require("dapui").setup() -- If using nvim-dap-ui
     end,
   },
   {
-    'stevearc/overseer.nvim',
+    "stevearc/overseer.nvim",
     opts = {},
   },
   {
-    'nvim-lua/plenary.nvim',
+    "nvim-lua/plenary.nvim",
   },
   {
-    'Civitasv/cmake-tools.nvim',
+    "Civitasv/cmake-tools.nvim",
     lazy = false,
-    ft = 'cmake',
+    ft = "cmake",
     opts = {},
   },
   {
     "rmagatti/auto-session",
-      lazy = false,
+    lazy = false,
 
-      ---enables autocomplete for opts
-      ---@module "auto-session"
-      ---@type AutoSession.Config
-      opts = {
-        suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-        -- log_level = 'debug',
-      }
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      -- log_level = 'debug',
+    },
   },
   {
-    'github/copilot.vim'
-  }
+    "github/copilot.vim",
+    lazy = false,
+  },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+      require("tiny-inline-diagnostic").setup {
+        options = {
+          add_messages = {
+            display_count = true,
+          },
+          multilines = {
+            enabled = true,
+          },
+        },
+      }
+      vim.diagnostic.config { virtual_text = false } -- Disable Neovim's default virtual text diagnostics
+    end,
+  },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
